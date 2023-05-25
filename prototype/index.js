4c708e49c18a854a1b1c78ec7e40dc38041005cf
Adriana Mihailescu <adriana.mihalescu@gmail.com>
2023-05-25 16:08:48 +0100

    add prototype design through serialization example

--- prototype/index.js
+++ prototype/index.js
@@ -5,10 +5,6 @@ class Address {
     this.country = country;
   }
 
-  deepCopy() {
-    return new Address(this.streetAddress, this.city, this.country);
-  }
-
   toString() {
     return `Address: ${this.streetAddress}, ` + `${this.city}, ${this.country}`;
   }
@@ -20,24 +16,76 @@ class Person {
     this.address = address; //!
   }
 
-  deepCopy() {
-    return new Person(
-      this.name,
-      this.address.deepCopy() // needs to be recursive
-    );
-  }
-
   toString() {
     return `${this.name} lives at ${this.address}`;
+  }
+
+  greet() {
+    console.log(
+      `Hi, my name is ${this.name}, ` + `I live at ${this.address.toString()}`
+    );
+  }
+}
+
+class Serializer {
+  constructor(types) {
+    this.types = types;
+  }
+
+  markRecursive(object) {
+    // anoint each object with a type index
+    let idx = this.types.findIndex((t) => {
+      return t.name === object.constructor.name;
+    });
+    if (idx !== -1) {
+      object["typeIndex"] = idx;
+
+      for (let key in object) {
+        if (object.hasOwnProperty(key) && object[key] != null)
+          this.markRecursive(object[key]);
+      }
+    }
+  }
+
+  reconstructRecursive(object) {
+    if (object.hasOwnProperty("typeIndex")) {
+      let type = this.types[object.typeIndex];
+      let obj = new type();
+      for (let key in object) {
+        if (object.hasOwnProperty(key) && object[key] != null) {
+          obj[key] = this.reconstructRecursive(object[key]);
+        }
+      }
+      delete obj.typeIndex;
+      return obj;
+    }
+    return object;
+  }
+
+  clone(object) {
+    this.markRecursive(object);
+    let copy = JSON.parse(JSON.stringify(object));
+    return this.reconstructRecursive(copy);
   }
 }
 
 let john = new Person("John", new Address("123 London Road", "London", "UK"));
 
-let jane = john.deepCopy();
+let jane = JSON.parse(JSON.stringify(john));
 
 jane.name = "Jane";
-jane.address.streetAddress = "321 Angel St"; // oops
+jane.address.streetAddress = "321 Angel St";
 
-console.log(john.toString()); // oops, john is called 'jane'
+john.greet();
+// this won't work
+// jane.greet();
+
+// try a dedicated serializer
+let s = new Serializer([Person, Address]); // pain point
+jane = s.clone(john);
+
+jane.name = "Jane";
+jane.address.streetAddress = "321 Angel St";
+
+console.log(john.toString());
 console.log(jane.toString());
